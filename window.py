ac34913cb12fe7f818a80b23a68ad203a1005c46
dady_ocat@hotmail.com <dady_ocat@hotmail.com>
2023-03-31 11:05:44 +0700

    lock size

--- window.py
+++ window.py
@@ -15,6 +15,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
+        MainWindow.setFixedSize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
